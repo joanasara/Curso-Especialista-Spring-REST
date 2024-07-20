7452f2c01bca1634054fedefd3c9f04f39e38832
Joana Sara <joana.leite@aluno.unifametro.edu.br>
2024-07-20 19:48:39 -0300

    Dependência opcional com @Autowired

--- algafood-api/src/main/java/com/algaworks/algafoodapi/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/service/AtivacaoClienteService.java
@@ -8,12 +8,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class AtivacaoClienteService {
 
-    @Autowired
+    @Autowired(required = false)
     private Notificador notificador;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+
+        if (notificador != null) {
+            notificador.notificar(cliente, "Seu cadastro no sistema esta ativo!");
+        } else {
+            System.out.println("Nao existe notificador, mas cliente foi ativado");
+        }
+        //notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
 
 }
